c39d0ea72741cf953074fd21de5c4088c06d28b6
VOELKJOS <josef.voelk@live.de>
2026-06-06 12:49:16 +0200

    Add React App component

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>OctoFit Tracker</h1>
+      <p>Welcome to your fitness tracking application</p>
+    </div>
+  )
+}
+
+export default App
